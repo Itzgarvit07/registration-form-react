f5602bbb2154a38f35a033429a75eaa8920f3089
Garvit <masterjangir7@gmail.com>
2024-03-27 19:18:16 +0530

    working on the aadhaar section

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -14,7 +14,7 @@ function Profile() {
 <div className='mt-5'>
    <div className='flex justify-evenly items-center'>
     <p>Aadhaar Card</p>
-    <form action="" className='border-2 border-black border-solid rounded' onClick={() =>document.querySelector('.input-field').click()}>
+    <form action="" className='border-2 border-gray-300 border-solid rounded' onClick={() =>document.querySelector('.input-field').click()}>
       <button className='bg-white  py-1 pl-2 text-left w-[350px] pr-[30px]'>Aadhar Card Front</button>
       <input type="file" accept='image/*' hidden className='input-field' onChange={({target:{files}}) =>{
 files[0] && setFileName(files[0].name)
@@ -22,16 +22,16 @@ if(files){
   setImage(URL.createObjectURL(files[0]))
 }
       }} />
-      <button className='bg-gray-300  py-1 px-2'>Browse</button>
+      <button className='bg-gray-200  py-1 px-2'>Browse</button>
 
       {image ?
     <img src={image} width={70} height={60} alt={fileName} /> : ""  
     }
     </form>
-    <form className='border-2 border-black border-solid rounded' action=""  onClick={() =>document.querySelector('.input-field').click()}>
+    <form className='border-2 border-gray-300 border-solid rounded' action=""  onClick={() =>document.querySelector('.input-field').click()}>
     <button className='bg-white  py-1 pl-2 text-left w-[350px] pr-[30px]'>Aadhar Card Back</button>
     <input type="file" accept='image/*' hidden className='input-field'/>
-    <button className='bg-gray-300 py-1 px-2'>Browse</button>
+    <button className='bg-gray-200 py-1 px-2'>Browse</button>
     </form>
    </div>
    </div>
