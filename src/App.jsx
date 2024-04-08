a96e1de0a423cdff155e9883f5db2195de087afe
Garvit <masterjangir7@gmail.com>
2024-04-08 15:23:23 +0530

    header completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 // import Newpassword from "./auth/Newpassword";
 
 // import RegisterForm from "./RegisterForm";
-import Login from "./auth/Login";
+// import Login from "./auth/Login";
 import Header from "./Header";
+// import TestingHeader from "./TestingHeader";
 // import Forgotpassword from "./auth/Forgotpassword";
 // import Profile from "./Profile";  
 // import Profile from "./Profile-section/Profile";
@@ -16,6 +17,7 @@ function App() {
       {/* <RegisterForm /> */}
       {/* <Login/> */}
       <Header/>
+{/* <TestingHeader/> */}
       {/* <Profile/> */}
       {/* <Profile/> */}
       {/* <Forgotpassword/> */}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,55 +1,58 @@
 import { Box } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 function Header() {
-  return (
-    
-<header>    
-<Box className="px-8" sx={{background:"linear-gradient(45deg,rgba(242,113,68,1) 63%, rgba(38,145,191,1) 63% )",width:"100%",height:"20px"}}>
-<Box className="px-8">
-    <Box className="px-9 text-sm text-white " sx={{display:"flex", justifyContent:"right", alignItems:"center"}}>
-<p className='mb-0 pr-5'>
-<i className="fa-solid fa-phone"></i>
-+91-8824453320
-</p>
-<p className='mb-0 pr-6'>
-<i className="fa-solid fa-phone"></i>
-+91-9024881021
-</p>
-</Box>
-</Box>
-</Box>
+    const [toggle, setToggle] = useState(false)
 
-<nav className='py-2 '>
-<Box sx={{display:"flex",justifyContent:"space-around", alignItems:"center"}}> 
-<Box sx={{height:"90px",width:"90px"}}>
-    <a href="#">
-        <img className='h-full w-full' src="./login-page-logo-1.png" alt="" />
-    </a>
-</Box>
-<Box>
-    <ul className='flex text-[#f47144] text-xl font-light'>
-        <li className='mx-5'><a className='hover:text-[#f47144]' href="#">Home</a></li>
-        <li className='mx-5'><a className='hover:text-[#f47144]' href="#">Courses</a></li>
-        <li className='mx-5'><a className='hover:text-[#f47144]' href="#">About</a></li>
-        <li className='mx-5'><a className='hover:text-[#f47144]' href="#">Blog</a></li>
-        <li className='mx-5'><a className='hover:text-[#f47144]' href="#">Contact Us</a></li>
-    </ul>
-</Box>
-<Box>
-    <button className='bg-[#f47144] py-1.5 px-3 text-white rounded-md hover:bg-[#2691bf] transition'><a className='font-bold text-white hover:text-white' href="#">Login/Register</a></button>
-</Box>
+    function onToggleMenu(e) {
+        e.preventDefault()
+        setToggle(!toggle)
+        console.log(e)
+    }
+    console.log(toggle,"12")
 
+    return (
+        <header>
+            <Box className="md:w-full  px-8" sx={{ background: "linear-gradient(45deg,rgba(242,113,68,1) 63%, rgba(38,145,191,1) 63% )", width: "100%",
+         }}>
+                <Box className="px-8">
+                    <Box className="md:text-sm text-[13px]  px-9  text-white " sx={{ display: "flex", justifyContent: "right", alignItems: "center" }}>
+                        <p className='mb-0 pr-5'>
+                            <i className="fa-solid fa-phone"></i>
+                            +91-8824453320
+                        </p>
+                        <p className='md:text-sm test-xs mb-0 pr-6'>
+                            <i className="fa-solid fa-phone"></i>
+                            +91-9024881021
+                        </p>
+                    </Box>
+                </Box>
+            </Box>
 
-</Box>
-
-
-
-
-</nav>
-
-</header>
-
+            <nav className='py-2 bg-white'>
+                <Box sx={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
+                    <Box sx={{ height: "90px", width: "90px" }}>
+                        <a href="#">
+                            <img className='h-full w-full' src="./login-page-logo-1.png" alt="" />
+                        </a>
+                    </Box>
+                    <Box className="md:static absolute bg-white md:min-h-fit min-h-[50vh]  right-[10%] md:w-auto w-full flex justify-end items-center  px-5 py-2" sx={{top:(toggle)? "25%" : "-100%" , transition:"0.9s ease-in",zIndex:(toggle)? "-3" :"1"}}>
+                        <ul className='flex md:flex-row flex-col items-center text-[#f47144] text-xl font-light md:gap-7 gap-8'>
+                            <li className=''><a className='hover:text-[#f47144]' href="#">Home</a></li>
+                            <li className=''><a className='hover:text-[#f47144]' href="#">Courses</a></li>
+                            <li className=''><a className='hover:text-[#f47144]' href="#">About</a></li>
+                            <li className=''><a className='hover:text-[#f47144]' href="#">Blog</a></li>
+                            <li className=''><a className='hover:text-[#f47144]' href="#">Contact Us</a></li>
+                        </ul>
+                    </Box>
+                    <Box className="flex items-center gap-6">
+                        <button className='bg-[#f47144] py-1.5 px-3 text-white rounded-md hover:bg-[#2691bf] transition'><a className='font-bold text-white hover:text-white' href="#">Login/Register</a></button>
+                        <i onClick={onToggleMenu} className="fa-solid fa-bars text-2xl cursor-pointer md:hidden"></i>
+                        {/* <i onClick={CloseMenu}  className="fa-solid fa-xmark text-2xl cursor-pointer md:hidden"></i> */}
+                    </Box>
+                </Box>
+            </nav>
+        </header>
     )
 }
 
